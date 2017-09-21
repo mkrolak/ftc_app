318ba815338c23da84876565c6ab75c9a2dbc03c
Axel <acsaupe@cps.edu>
2017-09-21 16:19:39 -0500

    Worked on direction method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import android.widget.Switch;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
@@ -13,6 +15,10 @@ public class Robot {
     public static final int RIGHT = 1;
     public static final int FORWARD = 2;
     public static final int BACK = 3;
+    public static final int FORWARDLEFT = 4;
+    public static final int FORWARDRIGHT = 5;
+    public static final int BACKLEFT = 6;
+    public static final int BACKRIGHT = 7;
     private DcMotor frontRightMotor;
     private DcMotor frontLeftMotor;
     private DcMotor backRightMotor;
@@ -32,8 +38,51 @@ public class Robot {
         //frontLeftMotor.setPower();
     }
 
-
+    //Select the direction for omni chasic
     public void setDirection(int direction){
+        //Test to see which direction with the 8 final
+        switch (direction){
+            case LEFT:
+                //frontLeftMotor.setDirection();
+                //frontRightMotor.setDirection();
+                //backLeftMotor.setDirection();
+                //backRightMotor.setDirection();
+                break;
+            case RIGHT:
+                //frontLeftMotor.setDirection();
+                //frontRightMotor.setDirection();
+                //backLeftMotor.setDirection();
+                //backRightMotor.setDirection();
+                break;
+            case FORWARD:
+                //frontLeftMotor.setDirection();
+                //frontRightMotor.setDirection();
+                //backLeftMotor.setDirection();
+                //backRightMotor.setDirection();
+                break;
+            case BACK:
+                //frontLeftMotor.setDirection();
+                //frontRightMotor.setDirection();
+                //backLeftMotor.setDirection();
+                //backRightMotor.setDirection();
+                break;
+            case FORWARDLEFT:
+                //frontRightMotor.setDirection();
+                //backRightMotor.setDirection();
+                break;
+            case FORWARDRIGHT:
+                //frontLeftMotor.setDirection();
+                //backLeftMotor.setDirection();
+                break;
+            case BACKLEFT:
+                //frontRightMotor.setDirection();
+                //backRightMotor.setDirection();
+                break;
+            case BACKRIGHT:
+                //frontLeftMotor.setDirection();
+                //backLeftMotor.setDirection();
+                break;
+        }
 
     }
 }
