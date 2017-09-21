37c0a6ed08cef99f40c39f066ced74fe4f927ea3
mkrolak <mkrolak@cps.edu>
2017-09-21 15:46:21 -0500

    I created two new classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTry.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by mkrol on 9/21/2017.
+ */
+
+@Autonomous
+public class FirstTry extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by mkrol on 9/21/2017.
+ */
+
+
+public class Robot {
+    private DcMotor motor;
+}
